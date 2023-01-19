67f911b8ed3615b1529aee5b81b640be124371bb
Anisha Dahal <anisha.dahal@webo.digital>
2023-01-20 02:08:30 +0545

    feat: local storage methods

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,128 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import { useState, useEffect } from "react";
 
-const Home: NextPage = () => {
+export default function Home() {
+  const [username, setUsername] = useState<string | null>(null);
+  const [userId, setUserId] = useState<string | null>(null);
+  const [userData, setUserData] = useState<{ email: string; age: number } | null>(null);
+  const [keys, setKeys] = useState<string[]>([]);
+  const [length, setLength] = useState<number>(0);
+
+  useEffect(() => {
+    if (typeof window !== "undefined" && window.localStorage) {
+      let username = localStorage.getItem("username");
+      let userId = localStorage.getItem("userId");
+      let userData = JSON.parse(localStorage.getItem("userData")!);
+      let keys: string[] = [];
+      for (let i = 0; i < localStorage.length; i++) {
+        keys.push(localStorage.key(i)!);
+      }
+      setUsername(username);
+      setUserId(userId);
+      setUserData(userData);
+      setKeys(keys);
+      setLength(localStorage.length);
+    }
+  }, []);
+
+  function handleSave() {
+    if (typeof window !== "undefined" && window.localStorage) {
+      localStorage.setItem("username", "Anisha");
+      localStorage.setItem("userId", "12345");
+      localStorage.setItem("userData", JSON.stringify({ email: "anisha@example.com", age: 25 }));
+
+      let username = localStorage.getItem("username");
+      let userId = localStorage.getItem("userId");
+      let userData = JSON.parse(localStorage.getItem("userData")!);
+      let keys: string[] = [];
+      for (let i = 0; i < localStorage.length; i++) {
+        keys.push(localStorage.key(i)!);
+      }
+
+      setUsername(username);
+      setUserId(userId);
+      setUserData(userData);
+      setKeys(keys);
+      setLength(localStorage.length);
+    }
+  }
+
+  function handleRemove() {
+    if (typeof window !== "undefined" && window.localStorage) {
+      localStorage.removeItem("username");
+      setUsername(null);
+      setKeys((keys) => keys.filter((k) => k !== "username"));
+      setLength(localStorage.length);
+    }
+  }
+
+  function handleClear() {
+    if (typeof window !== "undefined" && window.localStorage) {
+      localStorage.clear();
+      setUsername(null);
+      setUserId(null);
+      setUserData(null);
+      setKeys([]);
+      setLength(localStorage.length);
+    }
+  }
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+    <div className="h-screen bg-gray-200">
+      <h1 className="pt-4 text-5xl text-center">Local Storage Methods Demo</h1>
+      <div className="flex flex-col items-start py-4 mx-8">
+        <div>
+          <button
+            onClick={handleSave}
+            className="px-4 py-2 my-2 bg-blue-400 border-2 border-blue-500 rounded-lg hover:shadow-md hover:bg-blue-500"
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            Save
+          </button>
+          <span className="ml-1 mr-2 text-sm text-gray-800 ">
+            Store username, userId and userData key-value pairs
+          </span>
         </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+        <div>
+          <button
+            onClick={handleRemove}
+            className="px-4 py-2 my-2 bg-red-400 border-2 border-red-500 rounded-lg hover:shadow-md hover:bg-red-500"
+          >
+            Remove username
+          </button>
+          <span className="ml-3 mr-2 text-sm text-gray-800">Remove username key-value pair</span>
+        </div>
+        <div>
+          <button
+            onClick={handleClear}
+            className="px-4 py-2 my-2 bg-gray-400 border-2 border-gray-500 rounded-lg hover:shadow-md hover:bg-gray-500"
+          >
+            Clear
+          </button>
+          <span className="ml-3 mr-2 text-sm text-gray-800">Clear everything in localStorage</span>
+        </div>
+      </div>
+      <div className="items-start justify-between flex-1 h-screen mx-auto mt-6">
+        <hr className="border-gray-600 border-1" />
+        <div className="py-4 ml-8">
+          <h2 className="text-2xl">Output</h2>
+          <div>
+            <p className="p-2 text-sm">
+              Username: <span className="text-gray-800">{username}</span>
+            </p>
+            <p className="p-2 text-sm">
+              UserId: <span className="text-gray-800">{userId}</span>
+            </p>
+            <p className="p-2 text-sm">
+              UserData: <span className="text-gray-800">{JSON.stringify(userData)}</span>
+            </p>
+            <p className="p-2 text-sm">
+              List of Keys: <span className="text-gray-800">{keys.join(", ")}</span>
+            </p>
+            <p className="p-2 text-sm">
+              Total Items in Local Storage: <span className="text-gray-800">{length}</span>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
-
-export default Home
